5a842fc15fa5bfec1c395f5442f1a87055862130
Han-Jing Jiang <48014884+HanJingJiang@users.noreply.github.com>
2019-06-16 00:52:00 +0800

    Update SAE.py

--- SAE.py
+++ SAE.py
@@ -11,10 +11,10 @@ import math
 import random
 
 
-# 定义函数
+
 def ReadMyCsv(SaveList, fileName):
     csv_reader = csv.reader(open(fileName))
-    for row in csv_reader:  # 把每个rna疾病对加入OriginalData，注意表头
+    for row in csv_reader:  
         SaveList.append(row)
     return
 
@@ -25,33 +25,29 @@ def storFile(data, fileName):
     return
 
 
-# 读数据
+
 SampleFeature = []
 ReadMyCsv(SampleFeature, "SampleFeature.csv")
 SampleFeature = np.array(SampleFeature)
 print('SampleFeature',len(SampleFeature))
 print('SampleFeature[0]',len(SampleFeature[0]))
-x = SampleFeature #
-# from sklearn.cross_validation import train_test_split
-# x_train, x_test, y_train, y_test = train_test_split(x, x, test_size=0.2)    # 切分数据集进行训练，用全部数据集x进行“预测”！！！！
+x = SampleFeature 
 
-x_train = SampleFeature  # (3530, 1209)
+x_train = SampleFeature  
 x_test = SampleFeature
-# 改变数据类型
 x_train = x_train.astype('float32') / 1.
 x_test = x_test.astype('float32') / 1.
-print(x_train.shape)        # (3866, 906)
-print(x_test.shape)         # (3866, 906)
-print(type(x_train[0][0]))        # <class 'numpy.float32'>
+print(x_train.shape)      
+print(x_test.shape)        
+print(type(x_train[0][0]))       
 
-# 变量
 encoding_dim = 100
-input_img = Input(shape=(len(SampleFeature[0]),))    # 输入维度
+input_img = Input(shape=(len(SampleFeature[0]),))    
 
-# 构建autoencoder
+
 from keras import regularizers
 encoded_input = Input(shape=(encoding_dim,))
-encoded = Dense(encoding_dim, activation='relu', activity_regularizer=regularizers.l1(10e-7))(input_img)    # 与单层的唯一区别 (from keras import regularizers)!!!注意调节参数10e-7
+encoded = Dense(encoding_dim, activation='relu', activity_regularizer=regularizers.l1(10e-7))(input_img)    
 decoded = Dense(906, activation='sigmoid')(encoded)
 
 autoencoder = Model(inputs=input_img, outputs=decoded)
@@ -62,9 +58,8 @@ decoder = Model(inputs=encoded_input, outputs=decoder_layer(encoded_input))
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 autoencoder.fit(x_train, x_train, epochs=10, batch_size=50, shuffle=True, validation_data=(x_test, x_test))
 
-# 预测
 encoded_imgs = encoder.predict(x)
 decoded_imgs = decoder.predict(encoded_imgs)
-print(len(encoded_imgs))  #3866
-print(len(encoded_imgs[1])) #100
+print(len(encoded_imgs)) 
+print(len(encoded_imgs[1])) 
 storFile(encoded_imgs, '100SampleFeature.csv')
